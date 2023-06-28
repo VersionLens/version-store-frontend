f866db4c68218c955c1039ce0fcc13391ec6d8b3
VersionLens <support@versionlens.com>
2023-06-28 15:07:52 +0000

    Page: the landing page, Target: the main heading on the landing page, Change: change the text to "hello"

--- src/routes/(with-header-and-footer)/+page.svelte
+++ src/routes/(with-header-and-footer)/+page.svelte
@@ -21,7 +21,7 @@
 			class="text-4xl font-bold tracking-tight text-white lg:text-6xl"
 			data-ai-description="the main heading on the landing page"
 		>
-			New arrivals are here
+hello
 		</h1>
 		<p
 			class="mt-4 text-xl text-white"
