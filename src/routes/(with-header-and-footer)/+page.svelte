381a086802f5ac5d4604bc24ebffcdb11fac2d0f
VersionLens <support@versionlens.com>
2023-06-30 12:21:54 +0000

    Add new section for subscriptions at the bottom of the landing page

--- src/routes/(with-header-and-footer)/+page.svelte
+++ src/routes/(with-header-and-footer)/+page.svelte
@@ -239,4 +239,33 @@
 			</div>
 		</div>
 	</section>
+<section
+    aria-labelledby="subscriptions-heading"
+    class="mx-auto max-w-7xl py-24 px-4 sm:py-32 sm:px-6 lg:px-8"
+>
+    <div class="relative overflow-hidden rounded-lg">
+        <div class="absolute inset-0">
+            <img
+                src="https://tailwindui.com/img/ecommerce-images/home-page-01-feature-section-02.jpg"
+                alt=""
+                class="h-full w-full object-cover object-center"
+            />
+        </div>
+        <div class="relative bg-gray-900 bg-opacity-75 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
+            <div class="relative mx-auto flex max-w-3xl flex-col items-center text-center">
+                <h2 id="subscriptions-heading" class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                    Subscribe to our newsletter!
+                </h2>
+                <p class="mt-3 text-xl text-white">
+                    Stay up to date with our latest products and offers!
+                </p>
+                <a
+                    href="/subscribe"
+                    class="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
+                    >Subscribe</a
+                >
+            </div>
+        </div>
+    </div>
+</section>
 </main>
