6254e9af91461613ae05c123ab4a710ad6d4d4f1
VersionLens <support@versionlens.com>
2023-09-08 07:01:21 +0000

    Change background color of AddToBasketButton to purple

--- src/lib/components/basket/AddToBasketButton.svelte
+++ src/lib/components/basket/AddToBasketButton.svelte
@@ -17,6 +17,5 @@
 	on:click={() => addToBasket(productId)}
 	data-ai-description="the add to cart button in the product details"
 	type="submit"
-	class="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-	>Add to cart</button
->
+Add to cart
+</button>
