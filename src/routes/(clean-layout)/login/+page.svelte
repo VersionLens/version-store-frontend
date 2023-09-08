f6225cd2e095dbdefe9964ae84c6f7ea44ebbae4
VersionLens <support@versionlens.com>
2023-09-08 07:14:53 +0000

    Change button text to "All products here!" on landing page

--- src/routes/(clean-layout)/login/+page.svelte
+++ src/routes/(clean-layout)/login/+page.svelte
@@ -60,12 +60,12 @@
 				</div>
 
 				<div>
-					<button
-						on:click={() => alert('This is just a demo')}
-						type="submit"
-						class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-						>Sign in</button
-					>
+<button
+  on:click={() => alert('This is just a demo')}
+  type="submit"
+  class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  >All products here!</button
+>
 				</div>
 			</div>
 		</div>
