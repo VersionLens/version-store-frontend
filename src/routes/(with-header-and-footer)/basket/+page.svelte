c21fc04831f1993eb29e61018732be775e79de2e
VersionLens <support@versionlens.com>
2023-09-13 12:57:37 +0000

    Add number of items to shopping cart subtotal display

--- src/routes/(with-header-and-footer)/basket/+page.svelte
+++ src/routes/(with-header-and-footer)/basket/+page.svelte
@@ -104,8 +104,10 @@
 					<div>
 						<dl class="space-y-4">
 							<div class="flex items-center justify-between">
-								<dt class="text-base font-medium text-gray-900">Subtotal</dt>
-								<dd class="ml-4 text-base font-medium text-gray-900">€{getTotalAmount()}</dd>
+<dt class="text-base font-medium text-gray-900">Subtotal</dt>
+<dd class="ml-4 text-base font-medium text-gray-900">€{getTotalAmount()}</dd>
+<dt class="text-base font-medium text-gray-900">Number of Items</dt>
+<dd class="ml-4 text-base font-medium text-gray-900">{$basket.data.basket.items.length}</dd>
 							</div>
 						</dl>
 						<p class="mt-1 text-sm text-gray-500">
